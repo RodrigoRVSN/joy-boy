1a6fc58447b0ab2cbeff48db7b6fc3f603439b3b
RodrigoRVSN <rodrigovictor81@gmail.com>
2023-01-02 23:21:06 -0300

    chore: fix build erros in linter process

--- src/core/graphql/generated.ts
+++ src/core/graphql/generated.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,7 @@
-import { AppProps } from 'next/app'
-import '@App/core/styles/global.css'
-import { client } from '@App/core/lib/apollo'
 import { ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/app'
+import { client } from '@App/core/lib/apollo'
+import '@App/core/styles/global.css'
 
 function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
   return (
